9dabbc8d3d925f1a6b73a7c8f6a2b721a298603a
julien <frebet.julien@gmail.com>
2023-05-30 02:59:12 +0200

    feat(portfolio): add cv button ,start translation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,22 @@
 import { defineCollection, z } from 'astro:content';
 
-export const collections = {
-	work: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
-};
+const work = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()),
+    img: z.string(),
+    img_alt: z.string().optional(),
+  }),
+});
+
+const en = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
+export const collections = { work, en };
