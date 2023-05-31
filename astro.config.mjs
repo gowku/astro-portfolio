456dad2af6cc2a68be8d1b4f237350844eec8a0f
gowku <frebet.julien@gmail.com>
2023-05-31 20:33:54 +0200

    feat(portfolio): start translate projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-
 import robotsTxt from 'astro-robots-txt';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frebet-julien.gowkulab.ovh',
-  integrations: [react(), sitemap(), robotsTxt()],
+  integrations: [react(), sitemap(), robotsTxt(), mdx()]
 });
--- src/content/blabla/fr/contact.md
+++
@@ -1,9 +0,0 @@
----
-title: Contact | Julien Frébet
-description: Contact Julien Frébet
-heroTitle: Informations de contact
-tagline: Contactez-moi pour toute demande
-name: Nom
-placeholder: Bonjour ...
-send: Envoyer
----
