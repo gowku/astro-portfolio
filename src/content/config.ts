dbc385e97f7e47ecca57745a211d10f59896ad50
julien <frebet.julien@gmail.com>
2023-05-31 06:33:58 +0200

    feat(portfolio): add prettier,eslint

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
 const work = defineCollection({
   type: 'content',
@@ -8,34 +8,34 @@ const work = defineCollection({
     publishDate: z.coerce.date(),
     tags: z.array(z.string()),
     img: z.string(),
-    img_alt: z.string().optional(),
-  }),
-});
+    img_alt: z.string().optional()
+  })
+})
 
-const lang = defineCollection({
-  // type: 'content',
-  schema: z.object({
-    // en: z.object({
-    title: z.string().optional(),
-    description: z.string().optional(),
-    heroTitle: z.string().optional(),
-    tagline: z.string().optional(),
-    name: z.string().optional(),
-    email: z.string().optional(),
-    placeholder: z.string().optional(),
-    send: z.string().optional(),
+// const lang = defineCollection({
+//   // type: 'content',
+//   schema: z.object({
+//     // en: z.object({
+//     title: z.string().optional(),
+//     description: z.string().optional(),
+//     heroTitle: z.string().optional(),
+//     tagline: z.string().optional(),
+//     name: z.string().optional(),
+//     email: z.string().optional(),
+//     placeholder: z.string().optional(),
+//     send: z.string().optional()
 
-    // }),
-    // fr: z.object({
-    //   title: z.string(),
-    //   description: z.string(),
-    //   heroTitle: z.string().optional(),
-    //   tagline: z.string().optional(),
-    //   name: z.string().optional(),
-    //   placeholder: z.string().optional(),
-    //   send: z.string().optional(),
-    // }),
-  }),
-});
+//     // }),
+//     // fr: z.object({
+//     //   title: z.string(),
+//     //   description: z.string(),
+//     //   heroTitle: z.string().optional(),
+//     //   tagline: z.string().optional(),
+//     //   name: z.string().optional(),
+//     //   placeholder: z.string().optional(),
+//     //   send: z.string().optional(),
+//     // }),
+//   })
+// })
 
-export const collections = { work, lang };
+export const collections = { work }
