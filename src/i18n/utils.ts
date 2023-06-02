5814421860fa51efe49668f8b1ac0e19d20be612
gowku <frebet.julien@gmail.com>
2023-06-02 18:54:28 +0200

    fix(portfolio): lang in work

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,7 +3,8 @@
 import { ui, defaultLang } from './ui'
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/')
+  // const [, lang] = url.pathname.split('/')
+  const lang = url.pathname.split('/')[1]
   if (lang in ui) return lang as keyof typeof ui
   return defaultLang
 }
